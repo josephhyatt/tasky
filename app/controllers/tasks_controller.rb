f4b0b2bbc0461687007e2176786759baecf221ab
lord darkseid <github email address>
2018-08-12 12:24:04 -0700

    added flash, error messages and show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,11 +4,18 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  def create
-    @task = Task.new(tasks_params)
-    @task.save
-    redirect_to tasks_path(@task)
-  end
+	def create 
+		@task = current_user.tasks.new(tasks_params)
+		if @task.save
+			flash[:notice] = "Task was successfully created."
+			redirect_to tasks_path(@task)
+		else
+			render 'new'
+		end
+	end
+
+	def show 
+	end
 
   private
 
