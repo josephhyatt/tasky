8bfdc753dd450b0f6497b38684048c5b801135f8
lord darkseid <github email address>
2018-08-12 12:26:34 -0700

    added edit and update actions. created edit view file

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,20 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
-  end
+	end
+	
+	def edit
+	end
+
+	def create
+		@task = current_user.tasks.new(tasks_params)
+		if @task.save
+			flash[:notice] = "Task was successfully created."
+			redirect_to tasks_path(@task)
+		else
+			render 'new'
+		end
+	end
 
 	def create 
 		@task = current_user.tasks.new(tasks_params)
